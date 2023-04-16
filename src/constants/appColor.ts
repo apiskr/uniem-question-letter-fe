acb64f1d6acbe7550f6daaade7e4f51ce2442e27
Jchan <gkswocks123@gmail.com>
2023-04-16 16:39:37 +0900

    Feat : mui config 추가

---
+++ src/constants/appColor.ts
@@ -0,0 +1,22 @@
+const originColor = {
+  grey1: "#F2F2F2",
+  grey2: "#c8c8c8",
+  grey3: "#333333",
+  grey4: "#222222",
+  greyTransparent: "rgb(34,34,34,0.5)",
+  white: "#FFFFFF",
+  black: "#000000",
+  pastelYellow: "#F3EFE0",
+  turquoise: "#22A39F",
+  blue: "#1932B9",
+  lightBlue: "#7584D5",
+  greyBlue: "#4C5A67",
+};
+
+export const appColor = {
+  ...originColor,
+  backGround: originColor.grey1,
+  border: originColor.grey2,
+  text: originColor.grey3,
+  primary: originColor.greyBlue,
+};
