73ad9fe77874f739e82f2323ffb05eb1e81acfed
Jchan <gkswocks123@gmail.com>
2023-04-17 21:26:46 +0900

    Feat : sizes 상수 생성

---
+++ src/constants/sizes.ts
@@ -0,0 +1,10 @@
+import { breakPoints } from "./breakPoints";
+
+/**
+ * @description
+ * Specify if component sizes are used in duplicate
+ */
+
+export const sizes = {
+  pageMaxWidth: breakPoints.md - 500,
+};
