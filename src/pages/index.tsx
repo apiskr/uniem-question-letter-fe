0e2a2192c4539d1bff65946c6f221c8611791128
jungeol-h <dali2728@gmail.com>
2023-04-19 04:50:57 +0900

    [flx] vercel 오류 해결 div 태그를 mui 컴포넌트 box로 교체

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MainLayout } from "@/components/layouts/MainLayout";
-import { Stack, TextField, Typography } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import { Empty } from "@/components/atoms";
 import Image from "next/image";
 import billboard from "/public/billboard.png";
@@ -15,8 +15,8 @@ export default function Home() {
         {/* [Todo] Field 컴포넌트로 빼기 */}
         <Stack sx={{ width: "90%" }} spacing={2}>
           {/* 빌보드 */}
-          <div
-            style={{
+          <Box
+            sx={{
               position: "relative",
               display: "flex",
               width: "100%",
@@ -35,8 +35,8 @@ export default function Home() {
               fill
               sizes="(max-width:768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
-            <div
-              style={{
+            <Box
+              sx={{
                 position: "absolute",
                 fontWeight: 800,
                 fontSize: 28,
@@ -45,11 +45,11 @@ export default function Home() {
               }}
             >
               어려운 대학공부,<br></br>유니음에서 찾다
-            </div>
-          </div>
+            </Box>
+          </Box>
           {/* 기능1 */}
-          <div
-            style={{
+          <Box
+            sx={{
               // FeatureContainer
               display: "flex",
               flexDirection: "column",
@@ -59,16 +59,16 @@ export default function Home() {
               backgroundColor: "#ffffff",
             }}
           >
-            <div
-              style={{
+            <Box
+              sx={{
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "flex-start",
                 height: "100%",
               }}
             >
-              <div
-                style={{
+              <Box
+                sx={{
                   // Headline2
                   fontWeight: 800,
                   fontSize: 24,
@@ -78,12 +78,12 @@ export default function Home() {
                 }}
               >
                 시험공부, <br></br> 이미 A+ 받은 선배에게<br></br> 물어보세요
-              </div>
+              </Box>
               <Empty height="30px" />
               <Image src={feature1} width="250" alt="none" />
               <Empty height="30px" />
-              <div
-                style={{
+              <Box
+                sx={{
                   margin: "0",
 
                   // body1
@@ -94,12 +94,12 @@ export default function Home() {
                 과목 관련 질문을 하면 <br></br>
                 가장 잘 알고 있는 선배 멘토에게<br></br>
                 답변 받을 수 있습니다.
-              </div>
-            </div>
-          </div>
+              </Box>
+            </Box>
+          </Box>
           {/* 기능 2 */}
-          <div
-            style={{
+          <Box
+            sx={{
               // FeatureContainer
               display: "flex",
               flexDirection: "column",
@@ -109,16 +109,16 @@ export default function Home() {
               height: "100%",
             }}
           >
-            <div
-              style={{
+            <Box
+              sx={{
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "flex-start",
                 height: "657px",
               }}
             >
-              <div
-                style={{
+              <Box
+                sx={{
                   fontWeight: 800,
                   fontSize: 24,
                   lineHeight: "29px",
@@ -127,10 +127,10 @@ export default function Home() {
                 }}
               >
                 내가 배운 지식, <br></br>후배에게 원데이 클래스
-              </div>
+              </Box>
               <Empty height="11px" />
-              <div
-                style={{
+              <Box
+                sx={{
                   margin: "0",
                   // body1
                   fontSize: "15px",
@@ -138,12 +138,12 @@ export default function Home() {
                 }}
               >
                 역량 있는 멘토님들의<br></br> 원데이 클래스를 지원해드립니다.
-              </div>
-            </div>
-          </div>
+              </Box>
+            </Box>
+          </Box>
           {/* 회원가입 섹션 */}
-          <div
-            style={{
+          <Box
+            sx={{
               display: "flex",
               flexDirection: "column",
               justifyContent: "center",
@@ -154,8 +154,8 @@ export default function Home() {
               background: "#F9F9F9",
             }}
           >
-            <div
-              style={{
+            <Box
+              sx={{
                 // Headline 1
                 fontWeight: 800,
                 fontSize: 28,
@@ -167,14 +167,14 @@ export default function Home() {
             >
               대학생 지식 공유 플랫폼,<br></br>
               유니음
-            </div>
+            </Box>
             <Empty height="2rem" />
             {/* 버튼 */}
             <Image src={kakao_login_large_wide} width="300" alt="none" />
-          </div>
+          </Box>
           {/* Footer */}
-          <div
-            style={{
+          <Box
+            sx={{
               display: "flex",
               flexDirection: "column",
               alignItems: "flex-start",
@@ -186,8 +186,8 @@ export default function Home() {
             }}
           >
             {/* 유니음 관련 텍스트 */}
-            <div
-              style={{
+            <Box
+              sx={{
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "flex-start",
@@ -253,12 +253,12 @@ export default function Home() {
               >
                 dali1174@soongsil.ac.kr
               </p>
-            </div>
+            </Box>
             {/* [Todo] 짧은 라인 추가 */}
             <Empty height="10px" />
 
             {/* 아피스 관련 텍스트 */}
-            <div>
+            <Box>
               <p
                 style={{
                   margin: "0",
@@ -322,8 +322,8 @@ export default function Home() {
                 서울특별시 동작구 상도로 60가길 2, 201호(상도동)
               </p>
               <Empty height="10px" />
-            </div>
-          </div>
+            </Box>
+          </Box>
         </Stack>
         <Empty height="2rem" />
         <Stack sx={{ width: "90%" }} spacing={2}></Stack>
