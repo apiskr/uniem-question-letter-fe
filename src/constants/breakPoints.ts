2aa3b85c465c3616a2136d8dfa7b5943a98f0b6d
Jchan <gkswocks123@gmail.com>
2023-04-18 23:17:44 +0900

    Style : breakpoints.sm = 720

--- src/constants/breakPoints.ts
+++ src/constants/breakPoints.ts
@@ -1,6 +1,6 @@
 export const breakPoints = {
   xs: 0,
-  sm: 600,
+  sm: 720,
   md: 900,
   lg: 1200,
   xl: 1900,
--- src/constants/sizes.ts
+++ src/constants/sizes.ts
@@ -6,5 +6,5 @@ import { breakPoints } from "./breakPoints";
  */
 
 export const sizes = {
-  pageMaxWidth: breakPoints.md - 500,
+  pageMaxWidth: breakPoints.sm,
 };
