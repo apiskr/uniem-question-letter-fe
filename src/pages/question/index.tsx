defbb93ea8019f4c09b9210994ffd5e6a61a9346
Jchan <gkswocks123@gmail.com>
2023-04-22 00:13:32 +0900

    Remove

--- src/pages/question/index.tsx
+++ src/pages/question/index.tsx
@@ -1,39 +1,27 @@
 import React from "react";
 import { MainLayout } from "@/components/layouts/MainLayout";
-import { Stack, TextField, Typography } from "@mui/material";
-import { InputUploadImage } from "@/components/organisms/InputUploadImage";
-import { useWindowScreen } from "@/hooks/useWindowScreen";
+import { Stack, Button, Typography } from "@mui/material";
 import { Empty } from "@/components/atoms";
-import { MainTextField } from "@/components/atoms";
+import { QnATextField } from "@/components/organisms/QnATextField";
 
 export default function Question() {
-  const [uploadedFile, setUploadedFile] = React.useState<File | null>(null);
-  const { windowScreen } = useWindowScreen();
-
+  // [Todo] 피그마 컬러 theme 적용하기
+  // [Todo] 피그마 ui 작업하기
   return (
     <MainLayout>
       <MainLayout.Body>
-        {/* [Todo] Field 컴포넌트로 빼기 */}
         <Stack sx={{ width: "90%" }} spacing={2}>
           <Empty height="0.1rem" />
           <Typography variant="h3">질문 내용</Typography>
-          <MainTextField
-            multiline
-            rows={8}
-            placeholder="질문 내용을 입력해주세요."
-          />
-          <InputUploadImage
-            screen={windowScreen}
-            uploadedFile={uploadedFile}
-            setUploadedFile={setUploadedFile}
-          />
+          {/* [Todo] Input스타일링 없애고 Box안에다가 구현해야 할 듯 */}
+          <QnATextField multiline rows={8} placeholder="질문 내용을 입력해주세요." />
         </Stack>
         <Empty height="2rem" />
         <Stack sx={{ width: "90%" }} spacing={2}>
           <Typography variant="h3">질문 분류</Typography>
-          <TextField sx={{ width: "100%" }} placeholder="학과" />
-          <TextField sx={{ width: "100%" }} placeholder="과목" />
-          <TextField sx={{ width: "100%" }} placeholder="학년" />
+          <Button>학과</Button>
+          <Button>과목</Button>
+          <Button>학년</Button>
         </Stack>
       </MainLayout.Body>
     </MainLayout>
