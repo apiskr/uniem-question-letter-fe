a03d4ca1e3696e9d4fc4e6c5fa932b580a1f36f1
Jchan <gkswocks123@gmail.com>
2023-04-22 14:57:27 +0900

    Comment : Todo 코멘트 추가

--- src/pages/question/index.tsx
+++ src/pages/question/index.tsx
@@ -1,28 +1,27 @@
 import React from "react";
 import { MainLayout } from "@/components/layouts/MainLayout";
-import { Stack, Button, Typography } from "@mui/material";
+import { Button } from "@mui/material";
 import { Empty } from "@/components/atoms";
 import { QnATextField } from "@/components/organisms/QnATextField";
+import { SectionLayout } from "@/components/layouts/SectionLayout";
+import { ButtonSelect } from "@/components/atoms";
 
 export default function Question() {
-  // [Todo] 피그마 컬러 theme 적용하기
-  // [Todo] 피그마 ui 작업하기
+  // [Todo] 버튼 깜빡임 해결하기
+  // [Todo] 버튼 CSS 가끔 적용 안되는 문제 해결하기
   return (
     <MainLayout>
       <MainLayout.Body>
-        <Stack sx={{ width: "90%" }} spacing={2}>
-          <Empty height="0.1rem" />
-          <Typography variant="h3">질문 내용</Typography>
-          {/* [Todo] Input스타일링 없애고 Box안에다가 구현해야 할 듯 */}
+        <SectionLayout title="질문 내용">
           <QnATextField multiline rows={8} placeholder="질문 내용을 입력해주세요." />
-        </Stack>
+        </SectionLayout>
         <Empty height="2rem" />
-        <Stack sx={{ width: "90%" }} spacing={2}>
-          <Typography variant="h3">질문 분류</Typography>
-          <Button>학과</Button>
-          <Button>과목</Button>
-          <Button>학년</Button>
-        </Stack>
+        <SectionLayout title="질문 분류">
+          <ButtonSelect>학과</ButtonSelect>
+          <ButtonSelect>과목</ButtonSelect>
+          <ButtonSelect>학년</ButtonSelect>
+          <Button>ddd</Button>
+        </SectionLayout>
       </MainLayout.Body>
     </MainLayout>
   );
